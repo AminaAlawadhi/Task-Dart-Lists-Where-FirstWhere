2a41760445cdf022bb65f8463462dbeaa07b1439
AminaAlawadhi <amina7alawadhi@gmail.com>
2024-10-02 14:29:51 +0300

    initial commit

--- main.dart
+++ main.dart
@@ -1,5 +1,3 @@
-
-
 void main() {
   // Lists
   //1. Given this list of integers: `[5, 3, 15, 4, 1]`. Using a `forEach` method, count the sum of those elements.
@@ -21,14 +19,20 @@ void main() {
   //Where
   //1. Write a function called `filterVisitors` that takes 2 arguments, a list of ages and a minimum age allowed to enter.
   // Return a list of all ages that are more than the minimum age.
-  List<dynamic> ages = [10, 7, 22 , 18, 24];
-  int minAge=20;
+  List<dynamic> ages = [10, 7, 22, 18, 24];
+  int minAge = 20;
   List<dynamic> filterVisitors = ages.where((ages) => ages > minAge).toList();
   print(filterVisitors);
 
   //FirstWhere
   //1. Write a function called `findOdds` that takes a list of numbers and returns the first odd number.
-  //List<int> findOdds = [];
+  List<int> num = [3, 6, 11, 15, 20];
+  int? firstOdd = num.firstWhere((num) => num.isOdd, orElse: () => 0);
+  print(firstOdd);
+
   //orElse
   //1. Use the `orElse` named arguement with `FirstWhere` to return a `0` if no `odd` number is found.
+  List<int> noOddNum = [2, 4, 6, 8];
+  int oddNum = noOddNum.firstWhere((number) => number.isOdd, orElse: () => 0);
+  print(oddNum);
 }
